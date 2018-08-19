decce99b3dbe78c5e911586d0b66319cc4093b7a
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-08-19 17:02:33 -0400

    adding reversed numbers

---
+++ 0002_adding_reversed_numbers.cpp
@@ -0,0 +1,26 @@
+//https://www.spoj.com/problems/ADDREV/
+#include <iostream>
+using namespace std;
+
+int reverse(int n);
+
+int main() {
+    int numberOfCases;
+    cin >> numberOfCases;
+    int n1, n2;    
+    while (numberOfCases > 0) {
+        cin >> n1 >> n2;
+        cout << reverse(reverse(n1) + reverse(n2)) << endl;
+        numberOfCases--;
+	}
+    return 0;
+}
+
+int reverse(int n) {
+    int r = 0;
+    while (n > 0) {
+        r = (r * 10) + (n % 10);  
+        n = n / 10;
+    }
+    return r;
+}
