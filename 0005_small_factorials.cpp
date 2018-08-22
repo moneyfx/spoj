b3013651150645c787f4b43ecaad95b1a8b43acd
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-08-21 23:06:19 -0400

    small factorials

---
+++ 0005_small_factorials.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printFactorial(int n) {
+    //TODO
+    cout << endl;
+}
+
+int main() {
+    int i;
+    cin >> i;
+    while (i > 0) {
+        int n;
+        cin >> n;
+        printFactorial(n);
+        i--;
+    }
+    return 0;
+}
---
+++ 0005_small_factorials.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.math.BigInteger;
+
+class Main {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        while (i > 0) {
+            int n = scanner.nextInt();
+            System.out.println(getFactorial(n));
+            i--;
+        }
+        scanner.close();
+    }
+
+    public static BigInteger getFactorial(int n) {
+        BigInteger r = new BigInteger("1");
+        for (int i = 2; i <= n; i++) {
+            r = r.multiply(BigInteger.valueOf(i));
+        }
+
+        return r;
+    }
+}
